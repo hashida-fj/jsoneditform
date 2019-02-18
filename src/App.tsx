4e0afbf42f4c83350ce0e100c891638946372995
Takushi HASHIDA <hashida.takushi@jp.fujitsu.com>
2019-02-18 23:21:28 +0900

    added form and template

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
 import * as React from 'react';
 import './App.css';
 
+import * as njk from "nunjucks";
+
+import {JSONSchema6TypeName} from "json-schema";
+import Form, {IChangeEvent}from "react-jsonschema-form";
+
 import logo from './logo.svg';
+
+const schema = {
+  properties: {
+    done: {
+      default: false,
+      title: "Done?",
+      type: "boolean" as JSONSchema6TypeName,
+    },
+
+    title: {
+      default: "A new task",
+      title: "Title",
+      type: "string" as JSONSchema6TypeName,
+    },
+  },
+  required: ["title"],
+  title: "Todo",
+  type: "object" as JSONSchema6TypeName,
+};
+
+
+// const log = (type: string) => console.log(type);
+const template = njk.compile("{{ title }}, {{ done }}")
+const onSubmit = (e: IChangeEvent) => {
+  console.log(template.render(e.formData))
+}
 
 class App extends React.Component {
   public render() {
@@ -11,9 +42,10 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Form
+          schema={schema}
+          onSubmit={onSubmit}
+        />
       </div>
     );
   }
